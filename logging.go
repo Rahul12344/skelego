643331132d94f38f1586193673a4b18923baaac7
Rahul Natarajan <rahulnatarajan@Rahuls-MacBook-Pro-1453.local>
2021-04-29 16:48:57 -0700

    adding

--- logging.go
+++ logging.go
@@ -3,6 +3,9 @@ package skelego
 import (
 	"fmt"
 	"log"
+	"os"
+	"sync"
+	"time"
 )
 
 var (
@@ -10,29 +13,68 @@ var (
 )
 
 type logger struct {
-	name string
+	logfilename string
+	logfile     *os.File
+	log         *log.Logger
+	lock        sync.RWMutex
 }
 
 //NewLogger Creates a new logger - should have exactly one logger per application.
 func NewLogger() *logger {
+	file, _ := os.Create("logfile")
 	return &logger{
-		name: "Logger",
+		logfilename: "logfile",
+		logfile:     file,
+		log:         log.New(file, "recipes", log.Lshortfile),
 	}
 }
 
 //Logs event.
 func (logs *logger) LogEvent(val string, args ...interface{}) {
-	log.Printf(val, args...)
+	logs.lock.RLock()
+	logs.log.Printf(val, args...)
+	logs.lock.RUnlock()
 }
 
 //Logs non-fatal error.
 func (logs *logger) LogError(val string, args ...interface{}) {
-	log.Printf(fmt.Sprintf("Error: %s", val), args...)
+	logs.lock.RLock()
+	logs.log.Printf(fmt.Sprintf("Error: %s", val), args...)
+	logs.lock.RUnlock()
 }
 
 //Logs fatal event and terminates.
 func (logs *logger) LogFatal(val string, args ...interface{}) {
-	log.Fatalf(val, args...)
+	logs.lock.RLock()
+	logs.log.Fatalf(val, args...)
+	logs.lock.RUnlock()
+}
+
+// Perform the actual act of rotating and reopening file.
+func (logs *logger) Rotate() (err error) {
+	logs.lock.Lock()
+	defer logs.lock.Unlock()
+
+	// Close existing file if open
+	if logs.logfile != nil {
+		err = logs.logfile.Close()
+		logs.logfile = nil
+		if err != nil {
+			return
+		}
+	}
+	// Rename dest file if it already exists
+	_, err = os.Stat(logs.logfilename)
+	if err == nil {
+		err = os.Rename(logs.logfilename, logs.logfilename+"."+time.Now().Format(time.RFC3339))
+		if err != nil {
+			return
+		}
+	}
+
+	// Create a file.
+	logs.logfile, err = os.Create(logs.logfilename)
+	return
 }
 
 //New Logger
